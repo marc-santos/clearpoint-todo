e85e76cddd860afc3ce0da8eeb442d5a3757ff01
Daniel Nieuwoudt <daniel@xerabeth.com>
2024-06-25 16:10:20 +1200

    Centralise package management to make management easier across multiple projects.
    Update Dockerfile to restore packages based on the solution and not the project.

--- Backend/TodoList.Api/TodoList.Api/TodoItem.cs
+++ Backend/TodoList.Api/TodoList.Api/TodoItem.cs
@@ -6,7 +6,7 @@ namespace TodoList.Api
     {
         public Guid Id { get; set; }
 
-        public string Description { get; set; }
+        public string Description { get; set; } = string.Empty;
 
         public bool IsCompleted { get; set; }
     }
