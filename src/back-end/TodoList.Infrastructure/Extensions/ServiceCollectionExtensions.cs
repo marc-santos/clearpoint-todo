68bcf6168ec1c25991b37c38787690e61f1e9172
Daniel Nieuwoudt <daniel@xerabeth.com>
2024-07-05 17:21:09 +1200

    WIP: Add efcore and efcore migrations

---
+++ src/back-end/TodoList.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TodoList.Infrastructure.Data;
+
+namespace TodoList.Infrastructure.Extensions
+{
+    public static  class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<TodoListDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("SqlServer"),
+                    b => b.MigrationsAssembly(typeof(TodoListDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
